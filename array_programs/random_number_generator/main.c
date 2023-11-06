9f667909d36aaf5a689ba402bae432d0f2a65caf
Zesty Kun <adi8116315284@gmail.com>
2023-11-06 22:34:19 +0530

    Day 2 - Generating random numbers using rand() function

--- array_programs/random_number_generator/main.c
+++ array_programs/random_number_generator/main.c
@@ -1,8 +1,10 @@
+// Generate random numbers between 1 to 6
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main(){
-    srand(55);
-    int num = rand();
+    srand(time(0)); // Set the seed for rand() function as current time
+    int num = (rand() % 6) + 1; // Use the rand() function to generate a statistically random number
     printf("Pseudo-random number : %d",num);
     return 0;
 }
