05f7715f946b40f0aaf694b69221a9707c3f351d
Zesty Kun <adi8116315284@gmail.com>
2023-11-11 21:23:12 +0530

    Linked list

--- array_programs/deletion/main.c
+++ array_programs/deletion/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
     int a[100],i,n;
-    int b[5] = {1,2,3};
+    int b[5] = {1,2,3}; 
     n = 3;
+
+    // <!- Think about this later -->
     // printf("Enter the size of the array: ");
     // scanf("%d",&n);
     // printf("Enter the elements one by one : \n");
@@ -14,14 +16,20 @@ int main(){
     // for(i=0;i<n;i++){
     //     printf("a[%d] = %d \n",i,a[i]);
     // }
+    // <!-- -->
+
     printf("The element of the array before deletion are: \n");
-    for(i=0;i<n;i++){
+// Printing the elements of the array declared in the beginning
+    for(i=0;i<n;i++){   
         printf("b[%d] = %d \n",i,b[i]);
     }
+
+// Deleting the element <!- Research needed -->
     for(i=1;i<n;i++){
         b[i] = b[i+1];
         n = n - 1;
     }
+// Printing the elements of the array
     printf("The element of the array after deletion are: \n");
     for(i=0;i<n;i++){
         printf("b[%d] = %d \n",i,b[i]);
